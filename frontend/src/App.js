a82ff9dcca2ea6a886a732495d4f4de332aa6c8d
Pranab Kalita <pavankalita@gmail.com>
2022-11-25 19:10:41 +0530

    Created Product Listing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import HomePage from "./pages/HomePage";
+
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ReactShop</h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
