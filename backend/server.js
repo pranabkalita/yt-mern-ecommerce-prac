f5b8c84ec25f979b512c5df293026855e93e1291
Pranab Kalita <pavankalita@gmail.com>
2022-11-26 21:01:50 +0530

    Created the Custom Error Handler

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ import colors from "colors";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/product.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -18,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
